7602e112fc7a13b4fcc162aae0ffdea17d3a2f36
Vyacheslav Nagel <v9gel@mail.ru>
2024-07-15 15:48:11 +0700

    ios and material theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { App } from "konsta/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { useOperatingSystem } from "@siberiacancode/reactuse";
 
 export default function MyApp() {
+  const operatingSystem = useOperatingSystem();
+
   return (
-    <App theme="ios" className="fixed">
+    <App
+      theme={["ios", "macos"].includes(operatingSystem) ? "ios" : "material"}
+      className="fixed"
+    >
       <RouterProvider router={router} />
     </App>
   );
